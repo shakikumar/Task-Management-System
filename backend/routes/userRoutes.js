9ebb837eeffc866e3229b5a820af28e2b3f9e618
shakikumar <shakinanthakumar@gmail.com>
2026-06-08 18:47:10 +0530

    Add Swagger documentation for Member B CRUD APIs

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -19,18 +19,53 @@ const {
   deleteUser
 } = require('../controllers/userManagementController');
 
+/**
+ * @swagger
+ * /api/users:
+ *   post:
+ *     summary: Create user
+ *     tags: [Users]
+ */
 // POST /api/users → Create a new user
 router.post('/', protect, restrictTo('ADMINISTRATOR'), createUser);
 
+/**
+ * @swagger
+ * /api/users:
+ *   get:
+ *     summary: Get all users
+ *     tags: [Users]
+ */
 // GET /api/users → Get list of all users
 router.get('/', protect, restrictTo('ADMINISTRATOR'), getAllUsers);
 
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   get:
+ *     summary: Get user by ID
+ *     tags: [Users]
+ */
 // GET /api/users/:id → Get one specific user
 router.get('/:id', protect, restrictTo('ADMINISTRATOR'), getUserById);
 
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   put:
+ *     summary: Update user
+ *     tags: [Users]
+ */
 // PUT /api/users/:id → Update a user
 router.put('/:id', protect, restrictTo('ADMINISTRATOR'), updateUser);
 
+/**
+ * @swagger
+ * /api/users/{id}:
+ *   delete:
+ *     summary: Delete user
+ *     tags: [Users]
+ */
 // DELETE /api/users/:id → Delete a user
 router.delete('/:id', protect, restrictTo('ADMINISTRATOR'), deleteUser);
 
