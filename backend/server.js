e3e11e72534e214c0a1ba022ae289f6580baa7fc
HESHAN THEJANA <heshanthejana71@gmai.com>
2026-06-25 22:59:15 +0530

    Fix CORS for Netlify

--- backend/server.js
+++ backend/server.js
@@ -46,9 +46,12 @@ app.use(helmet());
 
 // This allows the frontend (React on port 3000) to send requests here
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    "http://localhost:5173",
+    "https://tasknova5.netlify.app"
+  ],
   credentials: true
-}));
+}))
 
 // This tells Express to understand JSON data sent from the frontend
 // Without this, req.body would be empty when frontend sends data
